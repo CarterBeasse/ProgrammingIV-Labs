4be7ac7fbf87c3db08779149ef3d41c94889eb79
unknown <196205081+CarterBeasse@users.noreply.github.com>
2026-03-31 14:13:52 -0400

    Finished my enumerator lab

--- Lab11/enumerator.py
+++ Lab11/enumerator.py
@@ -1,131 +1,15 @@
-from typing import TypeVar, Optional
-T = TypeVar("T")
-
 class Enumerate():
-    def __init__(self):
-        self.head: Optional[Link[T]] = None
-        self.obj: linked_list[T] = None
-        self.iter_loc = self.head
-
+    def __init__(self, obj):
+        self.obj = obj
+        self.count = -1
     def __iter__(self):
-        self.iter_loc = self.head
+        self.obj = iter(self.obj)
+        self.count += 1
         return self
-        pass
     def __next__(self):
-        if self.iter_loc is None:
-            raise StopIteration
-        res = self.iter_loc.element
-        self.iter_loc = self.iter_loc.next
-        return res
+        self.count += 1
+        return self.count,next(self.obj)
 
-class Link[T]:
-    """Stores a single element in a link 'chain'."""
-    def __init__(self, element:  T):
-        self.element: T = element
-        self.next: Optional[Link[T]] = None
-
-# class Prime100:
-#     def __init__(self):
-#         self.current = None
-#     def __next__(self):
-#         if self.current is None:
-#             self.current = 1
-#             return 2
-#         while True:
-#             self.current += 2
-#             if self.current > 100:
-#                 raise StopIteration
-#             if is_prime(self.current):
-#                 return self.current
-
-class linked_list[T]:
-    """A link chain implementation of a list."""
-
-    def __init__(self):
-        self.current = None
-        self.head: Optional[Link[T]] = None
-        self.tail: Optional[Link[T]] = None
-        self.iter_loc = self.head
-
-    def __next__(self):
-        if self.iter_loc is None:
-            raise StopIteration
-        res = self.iter_loc.element
-        self.iter_loc = self.iter_loc.next
-        return res
-
-    def __iter__(self):
-        self.iter_loc = self.head
-        return self
-
-    # Lab: complete these operations in order
-    def append(self, x: T):
-        new_link = Link(x)
-        if self.head is None: # If True head hasn't been set yet
-            self.head = new_link # First node is both head and tail
-        else:
-            self.tail.next = new_link # Move new node to end
-        self.tail = new_link # tail always moves to the newest node
-    def __setitem__(self, index: int, x: T):
-        pass
-
-    def __getitem__(self, index: int) -> T:
-        pass
-
-    def insert(self, index: int, x: T):
-
-        if index > len(self):
-            raise IndexError("Out of range")
-        new_link = Link(x)
-
-        if index == 0:
-            new_link.next = self.head
-            self.head = new_link
-            if self.tail is None:
-                self.tail = new_link
-            return
-
-        current = self.head
-
-        for _ in range(index - 1):
-            current = current.next
-        new_link.next = current.next
-        current.next = new_link
-
-        if new_link.next is None:
-            self.tail = new_link
-
-    def pop(self, index: int) -> T:
-        pass
-
-    def __len__(self) -> int:
-        count = 0
-        current = self.head
-        while current is not None:
-            count += 1
-            current = current.next
-        return count
-
-    def clear(self):
-        pass
-
-    def count(self, x: T) -> int:
-        pass
-
-    def index(self, x: T) -> int:
-        pass
-
-    def remove(self, x: T):
-        pass
-
-    # Challenge:
-
-    def reverse(self):
-        pass
-
-    def sort(self):
-        pass
-
-my_enumerate = Enumerate()
-
+for i in Enumerate([1, 5, 12]):
+    print(i)
 
