beab862b8bcea4e5355e030313de3562eebcd7e2
unknown <196205081+CarterBeasse@users.noreply.github.com>
2026-05-01 09:45:49 -0400

    Started mirroring a tree lab

--- lab14-Trees/Tree.py
+++ lab14-Trees/Tree.py
@@ -1,14 +1,28 @@
-class Tree:
-    def __init__(self):
-        self.parent = Node
-        self.children = [Node]
-
-    def create_tree(self, child):
-        self.parent = self
-        self.children.append(child)
-
 class Node:
-    def __init__(self):
-        self.data = Tree
-        self.left = Node
-        self.right = Node
+    def __init__(self,value):
+        self.value = value
+        self.parent = None
+        self.left = None
+        self.right = None
+root = Node(1)
+left = Node(2)
+right = Node(3)
+left_left = Node(4)
+left_right = Node(5)
+root.left = left
+root.right = right
+left.parent = root
+left.left = left_left
+left.right = left_right
+right.parent = root
+
+def mirror(self):
+    mirror_h(self.root)
+def mirror_h(current: Node):
+    if current is None:
+        return
+    print(current.value)
+    current.left = mirror_h(current.right)
+    current.right = mirror_h(current.left)
+
+mirror(root.value)
