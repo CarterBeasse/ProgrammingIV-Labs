77fe5ab5ee40c327889140ef0994dcb6bc881b23
unknown <196205081+CarterBeasse@users.noreply.github.com>
2026-05-04 14:04:12 -0400

    Made a count method for a binary tree

--- lab14-Trees/Tree.py
+++ lab14-Trees/Tree.py
@@ -16,7 +16,7 @@ left.left = left_left
 left.right = left_right
 right.parent = root
 
-def insert(node: Node, x):
+def insert(node: Node, x): #TODO: Add a helper method!!!!!!!!!!!!!!!!!!!!!!!!
     if node is None:
         return
     if x < node:
@@ -25,18 +25,20 @@ def insert(node: Node, x):
     # Change the new node
     # Recursively do insert
 
-def count(self):
-    pass
+# COUNT EACH NODE IN THE TREE
+def count(node:Node):
+    if node is None:
+        return 0
+    else:
+        return 1 + count(node.left) + count(node.right)
+print(count(root))
 
-def count_h(self, node):
-    pass
-
-def mirror(self):
-    mirror_h(self.root)
-def mirror_h(current: Node):
-    if current is None:
-        return
-    current.left,current.right = current.right,current.left
-    mirror_h(current.left)
-    mirror_h(current.right)
-mirror(root.value)
+# def mirror(self):
+#     mirror_h(self.root)
+# def mirror_h(current: Node):
+#     if current is None:
+#         return
+#     current.left,current.right = current.right,current.left
+#     mirror_h(current.left)
+#     mirror_h(current.right)
+# mirror(root.value)
