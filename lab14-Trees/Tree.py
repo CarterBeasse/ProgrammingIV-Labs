a75cb3f78ed7e58fc535f5b7c69da30b32ac9a31
unknown <196205081+CarterBeasse@users.noreply.github.com>
2026-05-04 13:29:45 -0400

    Attempted insert method for a Tree

--- lab14-Trees/Tree.py
+++ lab14-Trees/Tree.py
@@ -16,13 +16,27 @@ left.left = left_left
 left.right = left_right
 right.parent = root
 
+def insert(node: Node, x):
+    if node is None:
+        return
+    if x < node:
+        pass
+    # Add the value
+    # Change the new node
+    # Recursively do insert
+
+def count(self):
+    pass
+
+def count_h(self, node):
+    pass
+
 def mirror(self):
     mirror_h(self.root)
 def mirror_h(current: Node):
     if current is None:
         return
-    print(current.value)
-    current.left = mirror_h(current.right)
-    current.right = mirror_h(current.left)
-
+    current.left,current.right = current.right,current.left
+    mirror_h(current.left)
+    mirror_h(current.right)
 mirror(root.value)
